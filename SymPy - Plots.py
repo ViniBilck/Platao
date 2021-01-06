5fad6ce206677c40d2ca93d27a34a0d8690ae813
Vinicius Bilck <bilck.vinicius1998@gmail.com>
2021-01-06 18:03:56 -0300

    import

---
+++ SymPy - Plots.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+from sympy.plotting import plot 
+from sympy import * 
+t = input('Digite um valor inteiro \n' )
+t1 = int(t)
+x = Symbol("x")
+plot(sin(x)+t1)
+
+
+# In[ ]:
+
+
+
+
